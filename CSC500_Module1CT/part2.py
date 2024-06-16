2ee6a3a5918fddc2187dd66bbc3ccecf832b6d47
AFaurot <aaron.faurot@csuglobal.edu>
2024-06-16 12:21:38 -0600

    Added comments

--- CSC500_Module1CT/part2.py
+++ CSC500_Module1CT/part2.py
@@ -4,8 +4,9 @@ def main():
     # Gather user input
     num1 = int(input("Enter a whole number: \n"))
     num2 = int(input("Enter another whole number: \n"))
-    # Print product and quotient
+    # Print product
     print(num1, '*', num2, 'equals', num1 * num2)
+    # Print quotient after a conditional check for division by 0
     if num2 == 0:
         print(num1, '/', num2, 'is an undefined result (division by 0)')
     else:
