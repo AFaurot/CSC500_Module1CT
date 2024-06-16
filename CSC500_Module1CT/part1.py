d52b058a5249184a79a5e438d6c1826234704e7d
AFaurot <aaron.faurot@csuglobal.edu>
2024-06-16 12:11:20 -0600

    Ready for submission

---
+++ CSC500_Module1CT/part1.py
@@ -0,0 +1,12 @@
+# Part 1 CSC500 Module 1 Critical Thinking Assignment
+# Code by Aaron Faurot
+def main():
+    # Gather user input
+    num1 = int(input("Enter a whole number: \n"))
+    num2 = int(input("Enter another whole number: \n"))
+    # Print sum and difference
+    print(num1, '+', num2, 'equals', num1 + num2)
+    print(num1, '-', num2, 'equals', num1 - num2)
+
+
+if __name__ =='__main__': main()
---
+++ CSC500_Module1CT/part2.py
@@ -0,0 +1,15 @@
+# Part 2 CSC500 Module 1 Critical Thinking Assignment
+# Code by Aaron Faurot
+def main():
+    # Gather user input
+    num1 = int(input("Enter a whole number: \n"))
+    num2 = int(input("Enter another whole number: \n"))
+    # Print product and quotient
+    print(num1, '*', num2, 'equals', num1 * num2)
+    if num2 == 0:
+        print(num1, '/', num2, 'is an undefined result (division by 0)')
+    else:
+        print(num1, '/', num2, 'equals', num1 / num2)
+
+
+if __name__ == '__main__': main()
